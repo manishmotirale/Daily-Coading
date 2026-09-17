fbd741f723e9883a93a821cf048efa04851d55dd
Manish Motirale <manishmotirale@gmail.com>
2026-09-17 18:30:45 +0530

    Time: 7 ms (8.11%), Space: 15.6 MB (8.42%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> res;
-        int total = 1 << n;
-
-        for (int i = 0; i < total; i++) {
-            res.push_back(i ^ (i >> 1));
+        if (n == 0) return {0};
+        
+        // Get the sequence for n-1
+        vector<int> prev = grayCode(n - 1);
+        int offset = 1 << (n - 1); // 2^(n-1)
+        vector<int> res = prev;
+        
+        // Append the reversed prev sequence with the offset added
+        for (int i = prev.size() - 1; i >= 0; --i) {
+            res.push_back(prev[i] + offset);
         }
-
+        
         return res;
     }
 };
